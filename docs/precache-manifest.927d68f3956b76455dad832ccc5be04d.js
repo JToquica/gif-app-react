2c6556582b12f39acd56ff1c1f6ab162ff366a61
Toquica <jose.herley.2010@gmail.com>
2020-08-16 20:06:46 -0500

    docs agregado

---
+++ docs/precache-manifest.927d68f3956b76455dad832ccc5be04d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9344e689a12b00a51b4e1cffacfd86a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9d1c02c34a87e713d12",
+    "url": "/static/css/main.1d3c993a.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d1c02c34a87e713d12",
+    "url": "/static/js/main.f87260d1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
